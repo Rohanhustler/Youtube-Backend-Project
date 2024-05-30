2e6229923258dbd3e5efbc82e17e1d50e6021e18
Rohan Kumar Jha <jharohan0539@gmail.com>
2024-05-30 17:19:57 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -1,14 +1,38 @@
 // import mongoose from "mongoose";
 // import { DB_NAME } from "./constants";
-import connectDB from "./db/index.js";
+// import connectDB from "./db/index.js";
+import {app} from './app.js';
+import { asyncHandler } from './utils/asyncHanlder.js';
 
 
+const wrappedAsyncOperation = asyncHandler();
+wrappedAsyncOperation()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("Server Connection Error : ", error);
+    })
+    app.listen(process.env.PORT, () => {
+        console.log(`server is running on PORT ${process.env.PORT}`);
+    })
+})
+.catch((error) => {
+    console.log("ERRO", error);
+})
+
+/*
+connectDB()
+.then((response) => {
+    app.on("error", (error) => {
+        console.error("ERROR , Server is not responding currently, kindly check try to connect after sometimes", error);
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port : ${process.env.PORT}`)
+    })
+})
+.catch((error) => console.log("MONGO DB connection failed !!!", error))
 
 
-connectDB();
-
-
-
+*/
 /*
 import express from "express";
 const app = express();
---
+++ src/utils/asyncHanlder.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+import { app } from "../app.js";
+
+
+const asyncHanlder = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((error) => next(err));
+    }
+}
+/*
+function asyncHandler(fn) {
+    return async function(req, res, next) {
+        try{
+            await fn(req, res, next);
+        }
+        catch(error) {
+            res.status(error.code || 500).json({
+                success: false,
+                message: error.message
+            })
+        }
+    }
+}
+*/
+
+export {asyncHanlder};
