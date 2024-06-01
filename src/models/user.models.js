2337a5d9099fa7680879743e108da00695121ecc
Rohan Kumar Jha <jharohan0539@gmail.com>
2024-06-01 15:01:26 +0530

    user models changes

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -84,9 +84,5 @@ userSchema.methods.generateRefreshToken = function() {
     )
 }
 
-userSchema.methods.generateRefreshToken = function() {
-
-}
-
 
 export const User = mongoose.model("User", userSchema);
